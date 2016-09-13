9dda777e5d7e3bae8e6401edf154dc5ce25b36cf
Ferenc Béres <ferdzso05@gmail.com>
2016-09-13 14:05:58 +0000

    former popularity spearman experiment notebook refactored

--- python/correlation_new/correlation_utils_new.py
+++ python/correlation_new/correlation_utils_new.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 import correlation_computer as cc
 
+### Utils ###
+
 def load_score_map(input_prefix ,day, measure):
     """The centrality maps were pre-sorted in decreasing order!!!"""
     scores = pd.read_csv(input_prefix + '/%s_scores_%i.txt_s' % (measure,day), sep=" ", names=["id","score"])
@@ -39,4 +41,46 @@ def calculate_corr_for_a_day(input_prefix, day, corr_type, measure):
 def calculate_corr_for_days(input_prefix, days, corr_type, measure_type):
     """Calculate the selected correlation measure for multiple snapshots."""
     return map(lambda x: calculate_corr_for_a_day(input_prefix, x, corr_type=corr_type, measure=measure_type), days)
+
+### Popularity model ###
+
+import scipy.stats as stats
+
+def get_correlations_from_matrix(A, num_of_days, corr_type):
+    if corr_type=="pearson":
+        corr = map(lambda i : stats.pearsonr(A[i-1,:],A[i,:])[0], range(1,num_of_days))
+    elif corr_type=="spearman":
+        corr = map(lambda i : stats.spearmanr(A[i-1,:],A[i,:])[0], range(1,num_of_days))
+    elif corr_type=="kendall":
+        print "NOTE: Execution can be very slow for big vectors!"
+        corr = map(lambda i : stats.kendalltau(A[i-1,:],A[i,:])[0], range(1,num_of_days))
+    else:
+        raise RuntimeError("Invalid correlation type: %s!" % corr_type)
+    return corr
+
+def sort_and_get_corr(A, idx, corr_type):
+    two_day_df = pd.DataFrame(A[[idx-1,idx],:].T, columns=["prev","curr"]) # transposition is needed!
+    two_day_df["id"] = two_day_df.index
+    prev_df = two_day_df[["id","prev"]].query("prev>0")
+    curr_df = two_day_df[["id","curr"]].query("curr>0")
+    prev_df.sort_values("prev", ascending=False, inplace=True)
+    curr_df.sort_values("curr", ascending=False, inplace=True)
+    prev_day = prev_df.set_index("id")
+    current_day = curr_df.set_index("id")
+    if corr_type=="pearson":
+        corr = cc.corr_pearson(prev_day,current_day)[0]
+    elif corr_type=="spearman":
+        corr = cc.corr_spearman(prev_day,current_day)[0]
+    elif corr_type=="kendall":
+        corr = cc.corr_kendalltau(prev_day,current_day)[0]
+    elif corr_type=="w_kendall":
+        corr = cc.corr_weighted_kendalltau(prev_day,current_day)[0]
+    else:
+        raise RuntimeError("Invalid correlation type: %s!" % corr_type)
+    return corr
+    
+def get_correlations_from_matrix_for_act(A, num_of_days, corr_type):
+    """Function to call scipy correlation code for active vertices.
+    Matrix A contains scores from popularity model."""
+    return map(lambda i : sort_and_get_corr(A,i,corr_type=corr_type), range(1,num_of_days))
 
