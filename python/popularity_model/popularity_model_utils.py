b0ee0513c51e2cf085def4c5f7893cd7151fce74
Beres Ferenc <ferdzso05@gmail.com>
2017-02-12 23:28:24 +0000

    lambda stability nexperimenter notebooks added

--- python/popularity_model/popularity_model_utils.py
+++ python/popularity_model/popularity_model_utils.py
@@ -1,54 +1,37 @@
 import sys, math
+import pandas as pd
+import numpy as np
 from sklearn.metrics import mean_squared_error
 
-sys.path.insert(0,'../correlation')
-import correlation.correlation_utils as cu
+sys.path.insert(0,'../correlation_new')
+import correlation_new.correlation_utils_new as cu
 
-
-def filter_active_users(A, num_of_days):
-    num_users = A.shape[1]
-    centrality_maps = []
-    for i in range(num_of_days):
-        centrality_maps.append({})
-        for j in range(num_users):
-            val = A[i,j]
-            if val > 0.0:
-                centrality_maps[i][j] = val
-    return centrality_maps
-
-
-def get_custom_correlations(A, num_of_days, corr_type='spearman'):
-    """Return unweighted and weighted correlations"""
-    return cu.compute_correlation_sequential(filter_active_users(A,num_of_days),corr_type=corr_type)
-
+from popularity_model import PopularityModel
 
 def rmse(original_values, model_values):
     return math.sqrt(mean_squared_error(original_values, model_values))
 
+def test_stability_for_lambdas(lambdas, num_samples, corr_type, measure_id, popularity_model_parameters, origi_corrs, n_threads, output_prefix=None):
+    num_of_users, num_of_days, p, p_overlap = popularity_model_parameters
+    rmse_arr = []
+    for selected_lambda in lambdas:
+        for i in xrange(num_samples):
+            sample_model = PopularityModel(num_of_users, num_of_days)
+            scores_with_leaders = sample_model.get_centrality_with_markov(p, p_overlap, lambda_=selected_lambda)
+            simulated_corrs = cu.get_correlations_from_matrix_for_act(scores_with_leaders, num_of_days, corr_type, n_threads=n_threads)
+            rmse_arr.append([selected_lambda, corr_type, measure_id, rmse(origi_corrs, simulated_corrs)])
+            print i
+            if output_prefix != None:
+                rmse_df = pd.DataFrame(np.array(rmse_arr), columns=["lambda","corr_type","measure_id","rmse"])
+                rmse_df["rmse"] = rmse_df["rmse"].astype("float64")
+                rmse_df.to_csv("%s_stability.%s" % (output_prefix,corr_type),index=False)
+        print "lambda=%f finished" % selected_lambda
+    rmse_df = pd.DataFrame(np.array(rmse_arr), columns=["lambda","corr_type","measure_id","rmse"])
+    rmse_df["rmse"] = rmse_df["rmse"].astype("float64")
+    return rmse_df
 
-def get_result_for_lambda(proposed_lambda, pop_model, prob, prob_overlap, number_of_days, for_weighted):
-    scores_with_leaders = pop_model.get_centrality_with_markov(prob, prob_overlap, lambda_=proposed_lambda)
-    results = get_custom_correlations(scores_with_leaders, number_of_days)
-    if for_weighted:
-        model_spearman = list(results[:,1])
-    else:
-        model_spearman = list(results[:,0])
-    return model_spearman, scores_with_leaders
-
-
-def get_opt_lambda_for_model(proposed_lambdas, pop_model, prob, prob_overlap, number_of_days, original_corr_values, for_weighted=False):
-    opt_lambda = proposed_lambdas[0]
-    (opt_spearman, opt_scores) = get_result_for_lambda(opt_lambda, pop_model, prob, prob_overlap, number_of_days, for_weighted)
-    opt_diff = rmse(original_corr_values, opt_spearman)
-    #print opt_diff
-    for i in range(1, len(proposed_lambdas)):
-        current_lambda = proposed_lambdas[i]
-        (current_spearman, current_scores) = get_result_for_lambda(current_lambda, pop_model, prob, prob_overlap, number_of_days, for_weighted)
-        current_diff = rmse(original_corr_values, current_spearman)
-        #print current_lambda, current_diff
-        if current_diff < opt_diff:
-            opt_lambda = current_lambda
-            opt_spearman = current_spearman
-            opt_scores = current_scores
-            opt_diff = current_diff
-    return (opt_lambda, opt_spearman, opt_diff, opt_scores)
+def summarize_stability_results(rmse_df, key=["lambda","corr_type","measure_id"]):
+    mean_rmse = rmse_df.groupby(key).mean().reset_index()
+    std_rmse = rmse_df.groupby(key).std().reset_index()
+    rmse_stats = mean_rmse.merge(std_rmse,on=key,suffixes=("_mean","_std"))
+    return rmse_stats
